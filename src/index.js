f895e937137454145b57feefe0ec9748f2a77791
Rob LaFeve <rob@cremalab.com>
2016-05-27 09:18:53 -0500

    Add dev server

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import configureStore from './store/configureStore';
+
+class App extends React.Component {
+  render(){
+    return <h1>asdfj</h1>
+  }
+}
+
+render(
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
+  document.getElementById('app')
+)
